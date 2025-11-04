f33482a9704ef87efb97e11e1377a8a4b7103b1d
vijayshreekrishna <myworkat2023@gmail.com>
2025-11-04 09:02:19 +0530

    day1

---
+++ basic/main.js
@@ -0,0 +1,51 @@
+// //Strings , nums, boolens , null , undefinned
+
+const name = "VSK";
+const age = 21;
+  //
+  //concatenation
+  console.log("my name is " + name + " and age is " + age);
+  //
+  //template string
+ console.log(`my name is ${name} and i am ${age}`);
+
+
+
+//----------------------------------
+
+// const name ="VSK";
+// const age =21;
+// const isCool = true;
+// const rating =9;
+// const x= null;
+// const y  = undefined;
+// let z;
+
+
+// console.log(typeof name);
+// console.log(typeof age);
+// console.log(typeof isCool);
+// console.log(typeof rating);
+// console.log(typeof x);
+// console.log(typeof y);
+// console.log(typeof z);
+
+//----------------------------------
+
+// // var , let , const
+// let age =30;
+// age =31;
+// console.log(age);
+
+
+// const score =100;
+// console.log(score);
+
+//----------------------------------
+
+// //console.log("Hello, World!"); 
+// console.error("This is an error message by vsk.");
+// console.warn("This is a warning message by vsk.");
+// console.debug("This is a debug message by vsk.");
+// console.info("This is an info message by vsk.");
+
